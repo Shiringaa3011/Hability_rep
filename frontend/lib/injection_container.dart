7c50428d3c9f3721b1e57062b92d24fd8916dd12
Анастасия Ширинга <shiringaa3011@gmail.com>
2026-03-31 15:54:12 +0300

    feat: добавлена регистрация новых элементов

--- frontend/lib/injection_container.dart
+++ frontend/lib/injection_container.dart
@@ -18,12 +18,60 @@ import 'features/gamification/presentation/bloc/group_achievements/group_achieve
 import 'features/gamification/presentation/bloc/level/level_bloc.dart';
 import 'features/gamification/presentation/bloc/stats/stats_bloc.dart';
 
+import 'features/groups/data/repositories/group_repository_impl.dart';
+import 'features/groups/domain/repositories/group_repository.dart';
+import 'features/groups/domain/usecases/get_group_details.dart';
+import 'features/groups/domain/usecases/get_user_groups.dart';
+import 'features/groups/domain/usecases/join_group.dart';
+import 'features/groups/domain/usecases/leave_group.dart';
+import 'features/groups/domain/usecases/remove_member.dart';
+import 'features/groups/domain/usecases/send_reaction.dart';
+import 'features/groups/domain/usecases/create_group.dart';
+
+import 'features/home/data/repositories/home_repository_impl.dart';
+import 'features/home/domain/repositories/home_repository.dart';
+import 'features/home/domain/usecases/get_home_group_filter_options.dart';
+import 'features/home/domain/usecases/get_today_habits_for_day.dart';
+import 'features/home/domain/usecases/toggle_habit_completion.dart';
+import 'features/home/domain/usecases/get_habit_by_id.dart';
+import 'features/home/domain/usecases/upsert_habit_definition.dart';
+
+import 'features/notifications/data/repositories/notifications_repository_impl.dart';
+import 'features/notifications/domain/repositories/notifications_repository.dart';
+import 'features/notifications/domain/usecases/get_notification_history.dart';
+import 'features/notifications/domain/usecases/get_notification_settings.dart';
+import 'features/notifications/domain/usecases/mark_notification_read.dart';
+import 'features/notifications/domain/usecases/save_notification_settings.dart';
+
 final sl = GetIt.instance;
 
 Future<void> init() async {
   sl.registerLazySingleton<Dio>(() => createDioClient());
 
   sl.registerLazySingleton<HiveInterface>(() => Hive);
+
+  sl.registerLazySingleton<GroupRepository>(() => GroupRepositoryImpl());
+  sl.registerLazySingleton<HomeRepository>(() => HomeRepositoryImpl(sl()));
+  sl.registerLazySingleton<NotificationsRepository>(() => NotificationsRepositoryImpl());
+
+  sl.registerLazySingleton(() => GetUserGroups(sl()));
+  sl.registerLazySingleton(() => GetGroupDetails(sl()));
+  sl.registerLazySingleton(() => LeaveGroup(sl()));
+  sl.registerLazySingleton(() => RemoveMember(sl()));
+  sl.registerLazySingleton(() => SendReaction(sl()));
+  sl.registerLazySingleton(() => const JoinGroup());
+  sl.registerLazySingleton(() => CreateGroup(sl()));
+
+  sl.registerLazySingleton(() => GetTodayHabitsForDay(sl()));
+  sl.registerLazySingleton(() => GetHomeGroupFilterOptions(sl()));
+  sl.registerLazySingleton(() => ToggleHabitCompletion(sl()));
+  sl.registerLazySingleton(() => GetHabitById(sl()));
+  sl.registerLazySingleton(() => UpsertHabitDefinition(sl()));
+
+  sl.registerLazySingleton(() => GetNotificationHistory(sl()));
+  sl.registerLazySingleton(() => MarkNotificationRead(sl()));
+  sl.registerLazySingleton(() => GetNotificationSettings(sl()));
+  sl.registerLazySingleton(() => SaveNotificationSettings(sl()));
 
   await _initGamification();
 }
